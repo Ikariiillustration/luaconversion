9406a6c9700976d9a33bda8d1a5597ebc3fcc83e
Ikariiillustration <125511715+Ikariiillustration@users.noreply.github.com>
2023-05-03 22:55:22 -0500

    Add files via upload

---
+++ App_Start/FilterConfig.cs
@@ -0,0 +1,15 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using project2.Data;
+using project2.Models;
+
+namespace project2
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
